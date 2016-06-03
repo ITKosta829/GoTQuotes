0dc2a9f4d152d200d76cf5afe8205de54dba1516
Dean Siroky <dcsiroky@gmail.com>
2016-06-03 13:54:49 -0400

    Pushing my added files.

--- app/src/main/java/com/example/deanc/gotquotes/MainActivity.java
+++ app/src/main/java/com/example/deanc/gotquotes/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.deanc.gotquotes;
 
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.deanc.gotquotes.fragments.Shake;
+import com.example.deanc.gotquotes.fragments.TitlePage;
+
+import java.io.IOException;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    String url;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new TitlePage()).commit();
+
+        playMusic();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Second fragment after 7 seconds appears
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new Shake())
+                        .commit();
+            }
+        }, 7000);
+
+    }
+
+    public void playMusic() {
+
+        mediaPlayer = new MediaPlayer();
+        url = "https://soundcloud.com/fatihizm/game-of-thrones-main-theme";
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mediaPlayer.setDataSource(url);
+        } catch (IllegalArgumentException e) {
+
+        } catch (SecurityException e) {
+
+        } catch (IllegalStateException e) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mediaPlayer.prepare();
+        } catch (IllegalStateException e) {
+
+        } catch (IOException e) {
+
+        }
+
+        mediaPlayer.start();
+
     }
 }
---
+++ app/src/main/java/com/example/deanc/gotquotes/fragments/Shake.java
@@ -0,0 +1,27 @@
+package com.example.deanc.gotquotes.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.deanc.gotquotes.R;
+
+/**
+ * Created by DeanC on 6/3/2016.
+ */
+public class Shake extends android.support.v4.app.Fragment{
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        view = inflater.inflate(R.layout.title_page, null);
+
+        return view;
+    }
+}
