75373fc2105fdaa4ca69697d461fe4d8b2451453
Dean Siroky <dcsiroky@gmail.com>
2016-06-03 16:33:51 -0400

    added font and background image

--- app/src/main/java/com/example/deanc/gotquotes/QuoteView.java
+++ app/src/main/java/com/example/deanc/gotquotes/QuoteView.java
@@ -1,11 +1,14 @@
 package com.example.deanc.gotquotes;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +20,7 @@ public class QuoteView extends Fragment {
     View view;
     DataHandler dataHandler;
     Quote quote;
-
+    TextView charName, theQuote;
 
     @Nullable
     @Override
@@ -27,16 +30,21 @@ public class QuoteView extends Fragment {
         dataHandler = DataHandler.getInstance();
         quote = dataHandler.getSelectedQuote();
 
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "BLKCHRY.TTF");
+
         ImageView imageView = (ImageView) view.findViewById(R.id.characterFace);
         imageView.setImageResource(quote.getImageResource());
 
-        TextView charName = (TextView) view.findViewById(R.id.characterName);
+
+        charName = (TextView) view.findViewById(R.id.characterName);
         charName.setText(quote.getSayer());
+        charName.setTypeface(tf);
 
-        TextView theQuote = (TextView) view.findViewById(R.id.chosenQuote);
+        theQuote = (TextView) view.findViewById(R.id.chosenQuote);
         theQuote.setText(quote.getSaid());
-
+        theQuote.setTypeface(tf);
 
         return view;
     }
+
 }
