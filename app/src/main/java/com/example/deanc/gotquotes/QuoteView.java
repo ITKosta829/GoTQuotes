2c2309785d1e0b9f6906d863f37f6a58bac8b2f2
emerani <ezraerani@gmail.com>
2016-06-03 16:56:59 -0400

    final

--- app/src/main/java/com/example/deanc/gotquotes/QuoteView.java
+++ app/src/main/java/com/example/deanc/gotquotes/QuoteView.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,7 +39,7 @@ public class QuoteView extends Fragment {
         charName.setTypeface(tf);
 
         theQuote = (TextView) view.findViewById(R.id.chosenQuote);
-        theQuote.setText(quote.getSaid());
+        theQuote.setText( "\"" + quote.getSaid() + "\"");
         theQuote.setTypeface(tf);
 
         return view;
